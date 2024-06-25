aae960d5224d0ef3008b4b8a9f4d0e9d0d70c69a
Aran-tm <armandortmontes@gmail.com>
2024-06-25 14:17:39 -0500

    updating views and models

--- models/models.py
+++ models/models.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class leads_form(models.Model):
-#     _name = 'leads_form.leads_form'
-#     _description = 'leads_form.leads_form'
+class leads_form(models.Model):
+    _name = 'leads_form.leads_form'
+    _description = 'leads_form.leads_form'
 
-#     name = fields.Char()
+    name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
