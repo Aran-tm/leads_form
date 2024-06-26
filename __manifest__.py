62c9971866836e17058a9aa43a0ada04efb36e80
Aran-tm <armandortmontes@gmail.com>
2024-06-26 08:30:51 -0500

    creating leads assets

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
         This is an important module created for opportunities and leads at any enterprises""",
 
     'description': """
-        Long description of module's purpose
+        Module created for opportunities and leads at any enterprises
     """,
 
     'author': "Armando Roberto Travieso",
@@ -15,17 +15,15 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Leads',
+    'category': 'Learning/Learning',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'web'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
@@ -34,5 +32,10 @@
     # standard options for installation
     'installable': True,
     'application': True,
+    'assets': {
+        'web.assets': [
+
+        ]
+    },
     'auto_install': False,
 }
