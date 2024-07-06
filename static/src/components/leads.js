e96366c531823054bb1cd9948fd3b95a63a652f5
Aran-tm <armandortmontes@gmail.com>
2024-07-05 22:29:47 -0500

    obtaining data

--- static/src/components/leads.js
+++ static/src/components/leads.js
@@ -3,25 +3,7 @@
 import { registry } from "@web/core/registry"
 const { Component } = owl
 
-export class OwlLeadsForm extends Component {
-
-    // funcion que inicia cuando se renderiza el odoo 
-    // async willStart() {
-        
-    // }
-
-    /* 
-    
-    <input t-ref="miCampoEntrada" type="text" placeholder="Escribe algo...">
-
-    class MiComponente extends Component {
-        miMetodo() {
-            const valorDelCampo = this.$refs.miCampoEntrada.value;
-            console.log(valorDelCampo); // Imprime el valor del campo de entrada
-        }
-    }
-
-    */
+export class OwlLeadsForm extends Component {   
 
     // visualizar datos
     visualizarData = (event) => {
@@ -62,9 +44,35 @@ export class OwlLeadsForm extends Component {
 // nombre del template XML
 OwlLeadsForm.template = "owl.OwlLeadsForm"
 
+// registrar
+registry.category("actions").add("owl.leads_form", OwlLeadsForm)
+
 // AppComponent.components = {
 //     // Tus componentes secundarios aquí
 // };
 
-// registrar
-registry.category("actions").add("owl.leads_form", OwlLeadsForm)
+
+// funcion que inicia cuando se renderiza el odoo 
+    // async willStart() {
+        
+    // }
+
+    /* 
+    
+    <input t-ref="miCampoEntrada" type="text" placeholder="Escribe algo...">
+
+    class MiComponente extends Component {
+        miMetodo() {
+            const valorDelCampo = this.$refs.miCampoEntrada.value;
+            console.log(valorDelCampo); // Imprime el valor del campo de entrada
+        }
+    }
+
+        const nombre_apellidos = document.getElementById('nombre_apellidos').value;
+        const correo_electronico = document.getElementById('correo_electronico').value;
+        const telefono_personal = document.getElementById('telefono_personal').value;
+        const experiencia = document.getElementById('experiencia').value;
+        const fecha_nacimiento = document.getElementById('fecha_nacimiento').value;
+        const via_contacto = document.getElementById('via_contacto').value;
+
+    */
